2697451f0dbc11cbda9994c8c65cd6946a350b2c
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-03-12 00:36:43 +0300

    Update sfg.sh

--- sfg.sh
+++ sfg.sh
@@ -19,34 +19,36 @@ sudo grubby --args="gpu_sched.sched_policy=0" --update-kernel=ALL
 echo -e "\tNoiseTorch" && sudo dnf copr enable principis/NoiseTorch -y
 #Discord
 echo -e "\tDiscord" && sudo dnf config-manager --add-repo https://terra.fyralabs.com/terra.repo
-echo -e "\tmutter patch" && sudo dnf copr enable trixieua/mutter-patched -y
+echo -e "\tMutter Patch" && sudo dnf copr enable trixieua/mutter-patched -y
 #Install_Apps
 echo -e "\tInstall Apps" && sudo dnf in mangohud timeshift goverlay steam lutris transmission-gtk kdenlive vlc gnome-tweaks htop redhat-lsb-core rocm-opencl inxi neofetch protontricks openssl discord noisetorch easyeffects corectrl gimp google-chrome-stable openrgb piper --allowerasing
 sudo dnf update --refresh
 #Flatpak
 echo -e "\tFlatpak" && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && flatpak install flathub com.heroicgameslauncher.hgl com.obsproject.Studio com.mattjakeman.ExtensionManager
 #Disable_Services
-echo -e "\tOFF_Services" && sudo systemctl mask plymouth-quit-wait.service
-systemctl disable dmraid-activation.service
+echo -e "\tOFF_Services"
+sudo systemctl mask plymouth-quit-wait.service
 systemctl disable livesys-late.service
 systemctl disable livesys.service
 systemctl disable rpcbind.service
 systemctl disable lvm2-monitor.service
 systemctl disable NetworkManager-wait-online.service
-#Papirus_icon
-echo -e "\tPapirus icons"
-sudo dnf in papirus-icon-theme
-#corectrl
-sudo cp 90-corectrl.rules /etc/polkit-1/rules.d/
-#Fix_Volume_Step_for_Fifine_H6
-echo -e "Fix_Volume_Step_for_Fifine H6"
-gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 9
-#Fix_Suspend_for_Gigabyte_Aorus_Elite_v2_B550
-echo -e "Fix Suspend for Gigabyte Aorus Elite v2 B550"
+#GSConnect
+sudo firewall-cmd --zone=public --permanent --add-port=1714-1764/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=1714-1764/udp
+sudo systemctl restart firewalld.service
+#Fix Suspend for Gigabyte Aorus Elite v2 B550
 sudo cp wakeup-disable_GPP0.service /etc/systemd/system/
 sudo systemctl enable wakeup-disable_GPP0.service && sudo systemctl start wakeup-disable_GPP0.service
+#corectrl
+sudo cp 90-corectrl.rules /etc/polkit-1/rules.d/
+#Fix_Volume_Step
+gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 3
 #Fix_rgb_logitech_g213
 mkdir /home/bunnysword/.config/OpenRGB && cp 1.orp /home/bunnysword/.config/OpenRGB/
 #mkdir -p /home/bunnysword/.config/systemd/user/ && cp openrgb.service /home/bunnysword/.config/systemd/user/ && systemctl --user enable --now openrgb.service
 #other
 neofetch && cp config.conf /home/bunnysword/.config/neofetch/ && neofetch
+#Papirus_icon
+echo -e "\tPapirus icons"
+sudo dnf in papirus-icon-theme
