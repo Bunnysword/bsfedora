a7e778b0db6e680c1687911b5b585495a6526aa9
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-05-10 23:21:49 +0300

    Update sfg.sh

--- sfg.sh
+++ sfg.sh
@@ -38,3 +38,5 @@ mkdir /home/bunnysword/.config/OpenRGB && cp 1.orp /home/bunnysword/.config/Open
 #mkdir -p /home/bunnysword/.config/systemd/user/ && cp openrgb.service /home/bunnysword/.config/systemd/user/ && systemctl --user enable --now openrgb.service ?????????????????????
 #other
 neofetch && cp config.conf /home/bunnysword/.config/neofetch/ && neofetch && sudo dnf in -y papirus-icon-theme
+#steam-presence
+git clone https://github.com/JustTemmie/steam-presence && mv config.json steam-presence/ && cd steam-presence/ && ./installer.sh
