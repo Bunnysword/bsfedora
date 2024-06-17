2e77fe92c0f3dd94d86c06ee2c0d57e9e60c8215
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-06-17 13:09:43 +0300

    Update nvdriver.sh

--- nvdriver/nvdriver.sh
+++ nvdriver/nvdriver.sh
@@ -11,10 +11,10 @@ sudo dnf in https://koji.rpmfusion.org/kojifiles/packages/xorg-x11-drv-nvidia/55
 sudo dnf in https://koji.rpmfusion.org/kojifiles/packages/xorg-x11-drv-nvidia/555.42.02/1.fc41/x86_64/xorg-x11-drv-nvidia-power-555.42.02-1.fc41.x86_64.rpm https://koji.rpmfusion.org/kojifiles/packages/nvidia-modprobe/555.42.02/1.fc41/x86_64/nvidia-modprobe-555.42.02-1.fc41.x86_64.rpm 
 sudo dnf in https://koji.rpmfusion.org/kojifiles/packages/nvidia-settings/555.42.02/1.fc41/x86_64/nvidia-settings-555.42.02-1.fc41.x86_64.rpm https://koji.rpmfusion.org/kojifiles/packages/nvidia-persistenced/555.42.02/1.fc41/x86_64/nvidia-persistenced-555.42.02-1.fc41.x86_64.rpm
 ####555.52
-#sudo dnf install rpmfusion-nonfree-release-rawhide -y
+#sudo dnf in rpmfusion-nonfree-release-rawhide -y
 #sudo dnf --enablerepo=rpmfusion-nonfree-rawhide install akmod-nvidia-open xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda-libs.i686 --nogpgcheck 
-####
-#sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power
-#sudo dnf install xorg-x11-drv-nvidia-libs.i686
+####550
+#sudo dnf in gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power
+#sudo dnf in xorg-x11-drv-nvidia-libs.i686
 ####
 sudo cp nvidia-kms.conf /etc/modprobe.d/
